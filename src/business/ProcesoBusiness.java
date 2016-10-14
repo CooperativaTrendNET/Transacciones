7ae57d4a0e04019727f239e3a05561cb34f14abf
Daniel0694 <daniel_nuso@hotmail.com>
2016-10-13 19:54:53 -0600

    Se comenzó la clase que maneja la comunicación entre el cliente y el servidor

--- src/business/ProcesoBusiness.java
+++ src/business/ProcesoBusiness.java
@@ -1,6 +1,7 @@
 package business;
 
 import data.ProcesoData;
+import domain.Empleado;
 import domain.Transaccion;
 import java.sql.SQLException;
 import java.util.logging.Level;
@@ -12,7 +13,7 @@ import java.util.logging.Logger;
 public class ProcesoBusiness {
 
     private ProcesoData procesoData;
-    
+
     public ProcesoBusiness() {
         try {
             this.procesoData = new ProcesoData();
@@ -20,16 +21,16 @@ public class ProcesoBusiness {
             Logger.getLogger(ProcesoBusiness.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    public boolean credito_debito(Transaccion transaccion){
+
+    public boolean credito_debito(Transaccion transaccion) {
         boolean flag = false;
-        
+
         try {
             if (transaccion.getTipo().equalsIgnoreCase("deposito")) {
                 flag = this.procesoData.credito_debito(transaccion);
-            }else if(transaccion.getTipo().equalsIgnoreCase("retiro")){
+            } else if (transaccion.getTipo().equalsIgnoreCase("retiro")) {
                 flag = this.procesoData.credito_debito(transaccion);
-            }else{
+            } else {
                 System.err.println("No es un deposito ni retiro");
             }
         } catch (SQLException ex) {
@@ -38,21 +39,38 @@ public class ProcesoBusiness {
         }
         return flag;
     }
-    
-    public boolean transferencia(Transaccion transaccion){
+
+    public boolean transferencia(Transaccion transaccion) {
         boolean flag = false;
-        
+
         try {
             if (transaccion.getTipo().equalsIgnoreCase("transferencia")) {
                 flag = this.procesoData.transferencia(transaccion);
-            }else{
+            } else {
                 System.err.println("No es una trasferencia");
             }
         } catch (SQLException ex) {
             Logger.getLogger(ProcesoBusiness.class.getName()).log(Level.SEVERE, null, ex);
             flag = false;
         }
-        
+
         return flag;
+    }
+
+    public boolean loginCorrecto(String cuenta, String pass) {
+        try {
+            ProcesoData pd = new ProcesoData();
+            Empleado emp = new Empleado();
+            emp = pd.getEmpleado(cuenta);
+            if (emp.getContrasenia().equals(pass)) {
+                return true;
+            }
+
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(ProcesoBusiness.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return false;
+
     }
 }
--- src/logic/Comunicacion.java
+++ src/logic/Comunicacion.java
@@ -1,6 +1,101 @@
 package logic;
 
+import business.ProcesoBusiness;
+import data.ProcesoData;
+import domain.Empleado;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.BindException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JTextArea;
+import util.Strings;
 
-public class Comunicacion {
+public class Comunicacion implements Runnable {
 
+    private String numCuenta;
+    private String contrasennia;
+    private String tipo;
+    private float monto;
+    private String cuentaDestino;
+    private JTextArea jtaConsola;
+    private int puerto;
+    private boolean flag;
+
+    public Comunicacion() {
+        super();
+        this.puerto = 8080;
+        this.flag = true;
+    }
+
+    public void Hilo(JTextArea jtaConsola) {
+        this.jtaConsola = jtaConsola;
+    }
+
+    @Override
+    public void run() {
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(this.puerto);
+
+            do {
+                Socket socket = serverSocket.accept();
+                PrintStream enviar = new PrintStream(socket.getOutputStream());
+                BufferedReader recibir = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                this.tipo = recibir.readLine();
+
+                ProcesoData pd = new ProcesoData();
+                ProcesoBusiness pb = new ProcesoBusiness();
+
+                switch (this.tipo) {
+                    case "login":
+                        this.numCuenta = recibir.readLine();
+                        this.contrasennia = recibir.readLine();
+
+                        if (pb.loginCorrecto(this.numCuenta, this.contrasennia) == false) {
+                            enviar.println("false");
+
+                        } else {
+                            enviar.println("true");
+                        }
+                        break;
+
+                    case "consulta":
+                        this.numCuenta = recibir.readLine();
+                        Empleado emp = pd.getEmpleado(this.numCuenta);
+                        float fondo = emp.getFondo();
+
+                        enviar.println(fondo);
+
+                        break;
+
+                    case "deposito":
+                        this.numCuenta = recibir.readLine();
+                        this.monto = Float.parseFloat(recibir.readLine());
+                        
+                        break;
+
+                    case "retiro":
+
+                        break;
+
+                    case "transferencia":
+
+                        break;
+
+                }
+                socket.close();
+            } while (this.flag);
+        } catch (BindException e) {
+        } catch (IOException | SQLException | ClassNotFoundException ex) {
+            Logger.getLogger(Comunicacion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
 }
